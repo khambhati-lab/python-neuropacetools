c271dfc9273f124a6659bf017081d51d47410fcc
Ankit Khambhati <akhambhati@gmail.com>
2025-09-16 13:46:34 -0700

    Removed CatalogEntry definition from the function file.

--- src/neuropacetools/neuropaceraw/readers/ieegcat.py
+++ src/neuropacetools/neuropaceraw/readers/ieegcat.py
@@ -25,71 +25,9 @@ from typing import Union
 # Third-Party Packages #
 
 # Local Packages #
+from .catalogentry import CatalogEntry
 
 # Definitions #
-# Classes #
-class CatalogEntry(NamedTuple):
-    initials: str
-    patient_id: int
-    device_id: int
-    session_folder: str
-    filename: str
-    ecog_type: str
-    ecog_trigger: str
-    ecog_length: float
-    sampling_rate: float
-    waveform_count: int
-    ch_1_name: str
-    ch_2_name: str
-    ch_3_name: str
-    ch_4_name: str
-    ch_1_enabled: bool
-    ch_2_enabled: bool
-    ch_3_enabled: bool
-    ch_4_enabled: bool
-    timestamp_start:  datetime
-    timestamp_trigger: datetime
-
-    @classmethod
-    def from_csv(cls, _CatalogEntry):
-
-        # Convert to TZ-Aware
-        ts = datetime.fromisoformat(_CatalogEntry.timestamp)
-        raw_utc_ts = datetime.fromisoformat(_CatalogEntry.raw_utc_timestamp)
-        raw_local_ts = datetime.fromisoformat(_CatalogEntry.raw_local_timestamp)
-
-        raw_delta = raw_utc_ts.replace(tzinfo=None) - raw_local_ts
-        offset_hr = raw_delta.total_seconds() / 3600.0
-        offset_td = timedelta(hours=-1*offset_hr)
-        fixed_tz = timezone(offset_td)
-
-        ts_start = ts.replace(tzinfo=fixed_tz)
-        ts_trigger = raw_local_ts.replace(tzinfo=fixed_tz)
-
-        return cls(
-            str(_CatalogEntry.initials),
-            int(_CatalogEntry.patient_id),
-            int(_CatalogEntry.device_id),
-            str(_CatalogEntry.session_folder),
-            str(_CatalogEntry.filename),
-            str(_CatalogEntry.ecog_type),
-            str(_CatalogEntry.ecog_trigger),
-            float(_CatalogEntry.ecog_length),
-            float(_CatalogEntry.sampling_rate),
-            int(_CatalogEntry.waveform_count),
-            str(_CatalogEntry.ch_1_name),
-            str(_CatalogEntry.ch_2_name),
-            str(_CatalogEntry.ch_3_name),
-            str(_CatalogEntry.ch_4_name),
-            bool(_CatalogEntry.ch_1_enabled),
-            bool(_CatalogEntry.ch_2_enabled),
-            bool(_CatalogEntry.ch_3_enabled),
-            bool(_CatalogEntry.ch_4_enabled),
-            ts_start,
-            ts_trigger,
-        )
-
-
 # Functions #
 def _clean_header(s: str, sub: str='_', casing: str | None='lower') -> str:
     """Ensure header strings can serve as python identifiers"""
