83f77620f82695212388cda890ed388c05724ef3
Ankit Khambhati <akhambhati@gmail.com>
2025-09-16 13:47:26 -0700

    Explicit imports for CatalogEntry and IEEGRecord classes.

--- src/neuropacetools/neuropaceraw/readers/__init__.py
+++ src/neuropacetools/neuropaceraw/readers/__init__.py
@@ -14,6 +14,8 @@ __email__ = __email__
 
 # Imports #
 # Local Packages #
+from .catalogentry import CatalogEntry
 from .ieegdat import ieegdat
 from .ieegcat import ieegcat
 from .ieegget import ieegget
+from .ieegrecord import IEEGRecord
