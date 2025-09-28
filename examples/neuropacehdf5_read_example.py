31e046697e65cdec5baebf1019462a372edac46a
Ankit Khambhati <akhambhati@gmail.com>
2025-09-28 01:08:04 -0700

    Example how to read a file

--- examples/neuropacehdf5_read_example.py
+++ examples/neuropacehdf5_read_example.py
@@ -2,12 +2,6 @@
 from neuropacetools.neuropacehdf5.fileobjects import NEUROPACEHDF5
 
 ## primary
-loaded_file = NEUROPACEHDF5(file="example.h5", mode="r")
-print(loaded_file.attributes["file_version"])
-loaded_file.close()
-
-
-## alternative
-loaded_file = NEUROPACEHDF5(file="example2.h5", mode="r")
+loaded_file = NEUROPACEHDF5(file="133940083862290000.h5", mode="r")
 print(loaded_file.attributes["file_version"])
 loaded_file.close()
