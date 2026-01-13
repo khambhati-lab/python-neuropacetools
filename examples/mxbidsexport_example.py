67447fdc0c3b8f8a3cb9b86b2519e5e49b535a9b
Ankit Khambhati <akhambhati@gmail.com>
2026-01-13 08:50:46 -0800

    mxbidsexport_example.py

--- examples/mxbidsexport_example.py
+++ examples/mxbidsexport_example.py
@@ -1,4 +1,4 @@
-""" mxbidsexport_example.py.py
+""" mxbidsexport_example.py
 
 """
 # Imports #
@@ -22,46 +22,46 @@ def main():
 
     # Setup #
     # PATHS
-    # root_path = Path("/to/raw/neuropace/data/store")
-    # subject_identifier = "RAW_SUBJECT_ID"
-    mxbids_path = "./rns_subjects"
+    raw_path = Path("./raw_data")
+    subject_identifier = "ZZ"
+    mxbids_path = Path("./rns_subjects")
 
     ## Get Raw Data Objects
     ## Retrieve the catalog entry for a subject
-    catalog = neuropaceraw.ieegcat('./Example_ECoG_Catalog.csv')
+    catalog = neuropaceraw.ieegcat(raw_path / Path(f"{subject_identifier}_ECoG_Catalog.csv"))
 
+    # Create MXBIDS Subject
+    mxbids_subject = Subject(
+        name=subject_identifier,
+        parent_path=mxbids_path,
+        mode="w",
+        create=True
+    )
+
+    # Create MXBIDS Neuropace Session
+    ses_name = f"neuropace-{catalog[0].device_id}"
+    if (session := mxbids_subject.sessions.get(ses_name, None)) is None:
+        session = mxbids_subject.create_session(
+           session=NEUROPACEMXBIDSSession,
+           name=ses_name
+        )
+
+    # Create cdfs object
+    cdfs = session.modalities["ieeg"].components["cdfs"].require_cdfs()
+    cdfs.name = mxbids_subject.name if cdfs.name is None else cdfs.name
+
+    # create hdf5writer and contentupdater
+    hdf5writer = cdfs.components["contents"].create_data_writer()
+    contentsupdater = cdfs.components["contents"].create_contents_updater(
+        will_proxy=False,
+        init_setup=True
+    )
+
+    # Iterate over iEEG records listed in the catalog
     for cat in catalog:
-        ## Read the most recent catalog record entry
-        ieeg_record = neuropaceraw.ieegget('./', cat)
-        print(ieeg_record)
+        ## Read the most recent catalog record entry and retrieve iEEG record
+        ieeg_record = neuropaceraw.ieegget(raw_path, cat)
  
-        # Create MXBIDS Subject
-        mxbids_subject = Subject(
-            name=cat.initials,
-            parent_path=mxbids_path,
-            mode="w",
-            create=True
-        )
-
-        # Create MXBIDS Neuropace Session
-        ses_name = f"neuropace-{cat.device_id}"
-        if (session := mxbids_subject.sessions.get(ses_name, None)) is None:
-            session = mxbids_subject.create_session(
-               session=NEUROPACEMXBIDSSession,
-               name=ses_name
-            )
-    
-        # Create cdfs
-        cdfs = session.modalities["ieeg"].components["cdfs"].require_cdfs()
-        cdfs.name = mxbids_subject.name if cdfs.name is None else cdfs.name
-
-        # create hdf5writer and contentupdater
-        hdf5writer = cdfs.components["contents"].create_data_writer()
-        contentsupdater = cdfs.components["contents"].create_contents_updater(
-            will_proxy=False,
-            init_setup=True
-        )
-
         ## Convert the catalog entry to an hdf5 file
         filename = os.path.splitext(ieeg_record.catalog_entry.filename)[0]
         full_path, _ = cdfs.components["contents"].generate_file_path(filename)
@@ -72,9 +72,9 @@ def main():
             slice_=[None]
         )
 
+        ## Upsert entry into the cdfs
         entry = cdfs.components["contents"].format_entry(filename) 
         contentsupdater.evaluate(entry=entry)
-
 
 # Main #
 if __name__ == '__main__':
